3585aaf848a1433dd01d9df4db69c9d8ded25f15
tritri301 <tritri301@gmail.com>
2019-09-23 10:23:24 -0400

    Modified the query in our test to simulate the real db

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,9 +12,9 @@ public class main
                     "jdbc:mysql://10.20.40.40:3306/EquipeTristan_BD","BrainStorm","info420");
             //here sonoo is database name, root is username and password
             Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from test");
+            ResultSet rs=stmt.executeQuery("select * from ItemInfo");
             while(rs.next())
-                System.out.println(rs.getInt(1));
+                System.out.println(rs.getString("Description"));
             con.close();
         }catch(Exception e){ System.out.println(e);}
     }
