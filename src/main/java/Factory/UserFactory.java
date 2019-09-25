b20c2472f886cad4800911cb050a8ad36629a10f
Simon Gaudreault <SimonGaudreaults6@hotmail.com>
2019-09-25 10:15:12 -0400

    interface

--- src/main/java/Factory/UserFactory.java
+++ src/main/java/Factory/UserFactory.java
@@ -1,58 +1,17 @@
 package Factory;
 
+import Models.User;
+
 public class UserFactory {
+    public User create(short id,String nom,String password,String dateCreation,short acces){
+        User ret = new User();
 
-    Short idUser;
-    String nom;
-    String password;
-    String dateCreation;
-    Short acces;
+        ret.setIdUser(id);
+        ret.setNom(nom);
+        ret.setPassword(password);
+        ret.setDateCreation(dateCreation);
+        ret.setAcces(acces);
 
-    public UserFactory(Short idUser, String nom, String password, String dateCreation, Short acces) {
-        this.idUser = idUser;
-        this.nom = nom;
-        this.password = password;
-        this.dateCreation = dateCreation;
-        this.acces = acces;
-    }
-
-    public void setIdUser(Short idUser) {
-        this.idUser = idUser;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setDateCreation(String dateCreation) {
-        this.dateCreation = dateCreation;
-    }
-
-    public void setAcces(Short acces) {
-        this.acces = acces;
-    }
-
-    public Short getIdUser() {
-        return idUser;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getDateCreation() {
-        return dateCreation;
-    }
-
-    public Short getAcces() {
-        return acces;
+        return ret;
     }
 }
