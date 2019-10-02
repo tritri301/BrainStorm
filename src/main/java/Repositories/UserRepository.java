979dac534727aa322dc1b376c32d86bddae53d1f
tritri301 <tritri301@gmail.com>
2019-10-02 10:45:12 -0400

    Modified user repo

--- src/main/java/Repositories/UserRepository.java
+++ src/main/java/Repositories/UserRepository.java
@@ -3,32 +3,41 @@ package Repositories;
 import Repositories.Interfaces.UserRepositoryInterface;
 import Models.User;
 
+import java.util.List;
+
 public class UserRepository implements UserRepositoryInterface {
     private static final UserRepository instance = new UserRepository();
+
     @Override
-    public User FindById(int id) {
+    public User FindById(int id) throws Exception {
         return null;
     }
 
     @Override
-    public User[] FindAll() {
-        return new User[0];
-    }
-
-    @Override
-    public boolean Update(int id) {
-        return false;
-    }
-
-    @Override
-    public boolean Delete(int id) {
-        return false;
-    }
-
-    @Override
-    public User Create(User userToAdd) {
+    public List<User> FindByName(String name) throws Exception {
         return null;
     }
+
+    @Override
+    public List<User> FindAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void Update(User itemToUpdate) throws Exception {
+
+    }
+
+    @Override
+    public void Delete(int id) throws Exception {
+
+    }
+
+    @Override
+    public void Create(User itemToAdd) throws Exception {
+
+    }
+
     public static UserRepository GetInstance() {
         return instance;
     }
