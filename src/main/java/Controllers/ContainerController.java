ca59e5bcfd8e1a462d0340a4ba052140ae18f62d
Simon Gaudreault <gaus11079802@collegealma.ca>
2019-11-04 09:11:52 -0500

    ContainerController Terminé

--- src/main/java/Controllers/ContainerController.java
+++ src/main/java/Controllers/ContainerController.java
@@ -4,35 +4,99 @@ import Controllers.Interface.ContainerControllerInterface;
 import Controllers.Interface.ItemControllerInterface;
 import Models.Container;
 import Models.Item;
+import Services.ContainerService;
 import Services.ItemService;
-
 import java.util.List;
 import Exception.*;
+import View.Browser;
 
 
 public class ContainerController implements ContainerControllerInterface {
+
+    private static final ContainerController instance = new ContainerController();
+    ContainerService containerService = ContainerService.GetInstance();
+    Browser browser = Browser.GetInstance();
+
     @Override
-    public Container FindById(int id) {
-        return null;
+    public Container FindById(String emplacement)
+    {
+        Container container =  null;
+        try {
+            container = containerService.FindById(emplacement);
+        } catch (ExceptionCustom e) {
+            browser.Alert(e.getMessage());
+        }
+        catch(Exception e) {
+
+        }
+        return container;
     }
 
     @Override
     public List<Container> FindAll() {
-        return null;
+        List<Container> containerList = null;
+        try {
+            containerList = containerService.FindAll();
+        } catch (ExceptionCustom e) {
+            browser.Alert(e.getMessage());
+        }
+        catch(Exception e) {
+
+        }
+        return containerList;
     }
 
     @Override
-    public boolean Update(int idContainer, int quantite, int position, int volume, int poidsMax, int containerParent) {
-        return false;
+    public boolean Update(String emplacement, int volume, int volumeMax, int poids, int poidsMax, String emplacementParent) {
+        boolean update = true;
+
+        try {
+            update = containerService.Update(emplacement,volume,volumeMax,poids,poidsMax,emplacementParent);
+        } catch (ExceptionCustom e) {
+            browser.Alert(e.getMessage());
+            update = false;
+        }
+        catch(Exception e) {
+            update = false;
+        }
+        return update;
     }
 
     @Override
-    public boolean Create(int idContainer, int quantite, int position, int volume, int poidsMax, int containerParent) {
-        return false;
+    public boolean Create(String emplacement, int volume, int volumeMax, int poids, int poidsMax, String emplacementParent) {
+        boolean create = true;
+
+        try {
+            create = containerService.Create(emplacement,volume,volumeMax,poids,poidsMax,emplacementParent);
+        } catch (ExceptionCustom e) {
+            browser.Alert(e.getMessage());
+            create = false;
+        }
+        catch(Exception e) {
+            create = false;
+        }
+
+        return create;
     }
 
     @Override
-    public boolean Delete(int id) {
-        return false;
+    public boolean Delete(String emplacement) {
+        boolean delete = true;
+        try {
+            delete = containerService.Delete(emplacement);
+        } catch (ExceptionCustom e) {
+            delete = false;
+            browser.Alert(e.getMessage());
+        }
+        catch(Exception e) {
+            delete = false;
+        }
+
+        return delete;
+    }
+
+    public static ContainerController GetInstance()
+    {
+        return instance;
     }
 }
