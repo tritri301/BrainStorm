22f24d84ba446f56f2d981498473fdc229df6429
Jessica <jaycika87@hotmail.com>
2019-12-02 09:41:09 -0500

    Ajout de mon module

--- src/main/java/Repositories/ItemRepository.java
+++ src/main/java/Repositories/ItemRepository.java
@@ -5,10 +5,7 @@ import Models.ConnectionBD;
 import Models.Item;
 import Repositories.Interfaces.ItemRepositoryInterface;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
